833d397a797b78d0b461526ea7caa0a10537e366
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-16 22:51:47 +0300

    Completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+// Get the button element
+const myButton = document.querySelector('#my-button');
+
+// Define the event listener function
+function handleClick() {
+  console.log('Button clicked!');
 }
+
+// Bind the event listener to the button element
+myButton.addEventListener('click', handleClick);
